538b2eaf2a8d4cebea18bc0c50d5c100424c06da
macmade <macmade@xs-labs.com>
2015-01-09 14:34:50 +0100

    XS::Threading::Mutex compilation unit.

--- XS-C++/include/XS-C++/Threading/Mutex.h
+++ XS-C++/include/XS-C++/Threading/Mutex.h
@@ -28,53 +28,31 @@
  ******************************************************************************/
 
 /*!
- * @header      XS-C++.h
+ * @header      Mutex.h
  * @copyright   (c) 2015 - Jean-David Gadina - www.xs-labs.com
- * @abstract    XS-C++ main header file
+ * @abstract    Declaration of the XS::Threading::Mutex
  */
 
-#ifndef __XSCPP__
-#define __XSCPP__
-#ifdef  __cplusplus
+#ifndef __XSCPP_THREADING_MUTEX_H__
+#define __XSCPP_THREADING_MUTEX_H__
 
-/*!
- * @define      XSCPP_HAS_CPP11
- * @abstract    Whether the compiler has ISO C++ 2011 support
- */
-#if defined( __cplusplus ) && ( __cplusplus > 199711L || ( defined( _MSC_VER ) && _MSC_VER >= 1800 ) )
-#define XSCPP_HAS_CPP11         1
-#endif
+namespace XS
+{
+    namespace Threading
+    {
+        /*!
+         * @class           XS::Threading::Mutex
+         * @abstract        Mutex class
+         */
+        class Mutex: public XS::PIMPL::Object< Mutex >
+        {
+            public:
+                
+                using PIMPL::Object< Mutex >::impl;
+                
+                Mutex( bool recursive = false );
+        };
+    }
+}
 
-/*!
- * @define      XSCPP_THREADING_USE_STL
- * @abstract    Whether to use the ISO C++ 2011 thread support library
- * @discussion  If not defined previously to 0 (before inclusion of this header
- *              file, this macro will be defined to 1, meaning the ISO C++ 2011
- *              thread support library will be used instead of native thread
- *              functions (eg. pthread on POSIX).
- */
-#ifndef XSCPP_THREADING_USE_STL
-#define XSCPP_THREADING_USE_STL    1
-#endif
-
-/* ISO C++ 2011 is required */
-#ifndef XSCPP_HAS_CPP11
-#error "The XS-Labs C++ Utility Library requires ISO C++ 2011"
-#endif
-
-/* Includes from the C++ STL */
-#include <algorithm>
-#include <memory>
-#include <string>
-#include <iostream>
-
-/* Includes from XS-C++ */
-#include <XS-C++/Macros.h>
-#include <XS-C++/Types.h>
-#include <XS-C++/Atomic.h>
-#include <XS-C++/PIMPL/Object.h>
-#include <XS-C++/Version.h>
-#include <XS-C++/Threading/Mutex.h>
-
-#endif /* __cplusplus */
-#endif /* __XSCPP__ */
+#endif /* __XSCPP_THREADING_MUTEX_H__ */
