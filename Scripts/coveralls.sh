fe98bc36ecd3198fec2d20f8305f3b71fe43a280
Macmade <macmade@xs-labs.com>
2015-01-21 00:04:55 +0100

    Fixing coveralls script.

--- Scripts/coveralls.sh
+++ Scripts/coveralls.sh
@@ -2,4 +2,4 @@
 
 source Scripts/xcenv.sh
 declare -r DIR_BUILD="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-xcode-coveralls --include XS-C++ "${DIR_BUILD}"
+xcode-coveralls --include XS-C++ --project IDE/Xcode/XS-C++.xcodeproj "${DIR_BUILD}"
