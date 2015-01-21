49a1467d84146e30b15bd710b136b6e4cfa04751
Macmade <macmade@xs-labs.com>
2015-01-22 00:48:28 +0100

    Better code coverage reporting.

--- Scripts/coveralls.sh
+++ Scripts/coveralls.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-source Scripts/xcenv.sh
-declare -r DIR_BUILD="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-xcode-coveralls --include XS-C++ --project IDE/Xcode/XS-C++.xcodeproj "${DIR_BUILD}"
+source Scripts/xcenv-lib.sh
+declare -r DIR_BUILD_LIB="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+
+source Scripts/xcenv-tests.sh
+declare -r DIR_BUILD_TESTS="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+
+xcode-coveralls --include XS-C++ --project IDE/Xcode/XS-C++.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}"
