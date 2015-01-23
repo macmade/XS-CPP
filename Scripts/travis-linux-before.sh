018110ae0556a4d62cbbf8101e97191791e63b6f
Macmade <macmade@xs-labs.com>
2015-01-23 18:07:35 +0100

    Better Travis scripts.

--- Scripts/travis-linux-before.sh
+++ Scripts/travis-linux-before.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Installs dependancies
-sudo apt-get install gcc
-sudo apt-get install clang
-sudo apt-get install yasm
-sudo apt-get install make
-sudo apt-get install cmake
-sudo apt-get install libtool
-sudo apt-get install libc++1
-sudo dpkg --add-architecture i386
-sudo apt-get install libc++1:i386
-sudo dpkg --remove-architecture i386
+sudo apt-get -y install gcc
+sudo apt-get -y install g++
+sudo apt-get -y install clang
+sudo apt-get -y install yasm
+sudo apt-get -y install make
+sudo apt-get -y install cmake
+sudo apt-get -y install libtool
+sudo apt-get -y install libc++1
+sudo apt-get -y install libc++1:i386
+sudo apt-get -y install libc++-dev
+sudo apt-get -y install libc++-dev:i386
+sudo apt-get -y install libc6
+sudo apt-get -y install libc6:i386
+sudo apt-get -y install libc6-dev
+sudo apt-get -y install libc6-dev:i386
--- Scripts/travis-osx-after.sh
+++ Scripts/travis-osx-after.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Reports code coverage to coveralls.io
-bash Scripts/coveralls.sh
+source Scripts/xcenv-lib.sh
+declare -r DIR_BUILD_LIB="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+source Scripts/xcenv-tests.sh
+declare -r DIR_BUILD_TESTS="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+xcode-coveralls --include XS-C++ --project IDE/Xcode/XS-C++.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}"
