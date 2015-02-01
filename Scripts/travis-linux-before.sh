a3be9a5c1a0d171e58139c57e61f53077e41fafa
Macmade <macmade@ubuntu-precise.(none)>
2015-02-01 10:17:13 +0100

    Travis/Linux.

--- Scripts/travis-linux-before.sh
+++ Scripts/travis-linux-before.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Update packages
-sudo dpkg --add-architecture i386
-sudo apt-get update
+declare -r UBUNTU_14=`uname -v | grep trusty`
+
+if [ ! -z $UBUNTU_14 ]; then
+    sudo dpkg --add-architecture i386
+    sudo apt-get update
+fi
 
 # Installs dependancies
 sudo apt-get -y install make
 sudo apt-get -y install clang
-sudo apt-get -y install libc++1
-sudo apt-get -y install libc++1:i386
-sudo apt-get -y install libc++-dev
-sudo apt-get -y install libc++-dev:i386
 sudo apt-get -y install libc6
 sudo apt-get -y install libc6:i386
 sudo apt-get -y install libc6-dev
 sudo apt-get -y install libc6-dev:i386
+
+if [ ! -z $UBUNTU_14 ]; then
+    sudo apt-get -y install libc++1
+    sudo apt-get -y install libc++1:i386
+    sudo apt-get -y install libc++-dev
+    sudo apt-get -y install libc++-dev:i386
+else
+    export LIBRARY_PATH=/usr/lib/i386-linux-gnu/
+    export C_INCLUDE_PATH=/usr/include/i386-linux-gnu
+    export CPLUS_INCLUDE_PATH=/usr/include/i386-linux-gnu
+fi
