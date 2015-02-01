16f1c4aa9829514893f6600e804ff6f1cbe1fa96
Macmade <macmade@ubuntu-precise.(none)>
2015-02-01 10:26:01 +0100

    Travis/Linux

--- Scripts/travis-linux-before.sh
+++ Scripts/travis-linux-before.sh
@@ -7,6 +7,25 @@ if [ ! -z $UBUNTU_14 ]; then
     sudo apt-get update
 fi
 
+if [ ! -z $UBUNTU_14 ]; then
+    sudo apt-get -y install libc++1
+    sudo apt-get -y install libc++1:i386
+    sudo apt-get -y install libc++-dev
+    sudo apt-get -y install libc++-dev:i386
+else
+    sudo apt-get -y install binutils
+    sudo apt-get -y install binutils:i386
+    sudo apt-get -y install gcc-4.6
+    sudo apt-get -y install gcc-4.6:i386
+    sudo apt-get -y install libstdc++6.4.6
+    sudo apt-get -y install libstdc++6.4.6:i386
+    sudo apt-get -y install libstdc++6.4.6-dev
+    sudo apt-get -y install libstdc++6-4.6-dev:i386
+    export LIBRARY_PATH=/usr/lib/i386-linux-gnu/
+    export C_INCLUDE_PATH=/usr/include/i386-linux-gnu
+    export CPLUS_INCLUDE_PATH=/usr/include/i386-linux-gnu
+fi
+
 # Installs dependancies
 sudo apt-get -y install make
 sudo apt-get -y install clang
@@ -14,14 +33,3 @@ sudo apt-get -y install libc6
 sudo apt-get -y install libc6:i386
 sudo apt-get -y install libc6-dev
 sudo apt-get -y install libc6-dev:i386
-
-if [ ! -z $UBUNTU_14 ]; then
-    sudo apt-get -y install libc++1
-    sudo apt-get -y install libc++1:i386
-    sudo apt-get -y install libc++-dev
-    sudo apt-get -y install libc++-dev:i386
-else
-    export LIBRARY_PATH=/usr/lib/i386-linux-gnu/
-    export C_INCLUDE_PATH=/usr/include/i386-linux-gnu
-    export CPLUS_INCLUDE_PATH=/usr/include/i386-linux-gnu
-fi
